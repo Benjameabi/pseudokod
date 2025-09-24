c8c78467f01101345c87b9fe0ecbfe0ad07010b0
Benjame Abi <benjameabi@gmail.com>
2025-09-24 10:33:14 +0200

    Pseudokod

---
+++ main.js
@@ -0,0 +1,12 @@
+//KATA: Students Final Grade
+//---------------------------
+// GOAL: Calculate the final grade based on tests, results, and submitted projects.
+// INPUTS: Exam grades, and projects completed. Positive integers only.
+// RETURN: Either one of these positive integers: 100, 90, 75, 0.
+// STEP 1: Have a function that argues two parameters: exam (number), projects (number).
+// STEP 2: If exam grade is >90 or number of completed projects is >10. If true, return 100.
+// STEP 3: If exam grade is >75 and project number is >=5. If true, return 90.
+// STEP 4: If exam grade is >50 and project number is >=2. If true, return 75.
+// STEP 5: If steps 2-4 are false, return 0
+// Done!!
+
